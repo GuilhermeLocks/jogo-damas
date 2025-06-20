446428437fcd6c46b80c6dffe52c17ca40ffa568
Guilherme da Silva Locks <guisilvalocks@gmail.com>
2025-06-19 23:13:21 -0300

    Update jogo v1.0.py

--- jogo v1.0.py
+++ jogo v1.0.py
@@ -9,8 +9,7 @@ tabuleiro = [
     [fundo, 4, 1], [fundo, 4, 2], [fundo, 4, 3], [fundo, 4, 4], [fundo, 4, 5], [fundo, 4, 6], [fundo, 4, 7], [fundo, 4, 8],
     [jogador, 3, 1], [fundo, 3, 2], [jogador, 3, 3], [fundo, 3, 4], [jogador, 3, 5], [fundo, 3, 6], [jogador, 3, 7], [fundo, 3, 8],
     [fundo, 2, 1], [jogador, 2, 2], [fundo, 2, 3], [jogador, 2, 4], [fundo, 2, 5], [jogador, 2, 6], [fundo, 2, 7], [jogador, 2, 8],
-    [jogador, 1, 1], [fundo, 1, 2], [jogador, 1, 3], [fundo, 1, 4], [jogador, 1, 5], [fundo, 1, 6], [jogador, 1, 7], [fundo, 1, 8]
-             ]
+    [jogador, 1, 1], [fundo, 1, 2], [jogador, 1, 3], [fundo, 1, 4], [jogador, 1, 5], [fundo, 1, 6], [jogador, 1, 7], [fundo, 1, 8]]
 
 posicao = 0
 for c in tabuleiro:
@@ -20,8 +19,6 @@ for c in tabuleiro:
     print(c[0], end='')
 print('')
 print('')
-
-
 
 posicao = 0
 while posicao == 0:
@@ -33,12 +30,11 @@ while posicao == 0:
                 if c[0] == 'o ':
                     posicao = c
                     c[0] = '\033[35mo \033[m'
+                    print(c)
                 else:
                     print('')
                     print('Posição invalida.')
                     print('')
-
-
 
 posicao = 0
 for c in tabuleiro:
