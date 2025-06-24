2a3b335be4b1d7f4d2cfe24f5a0374925b85c3fd
Guilherme da Silva Locks <guisilvalocks@gmail.com>
2025-06-24 19:19:35 -0300

    Update jogo v1.0.py

--- jogo v1.0.py
+++ jogo v1.0.py
@@ -2,6 +2,7 @@ fundo_jogavel = '=  '
 fundo_injogavel = '*  '
 jogador = 'o  '
 computador = 'x  '
+posicao = 0
 tabuleiro = [
     [fundo_jogavel, 8, 1], [computador, 8, 2], [fundo_jogavel, 8, 3], [computador, 8, 4], [fundo_jogavel, 8, 5], [computador, 8, 6], [fundo_jogavel, 8, 7], [computador, 8, 8],
     [computador, 7, 1], [fundo_jogavel, 7, 2], [computador, 7, 3], [fundo_jogavel, 7, 4], [computador, 7, 5], [fundo_jogavel, 7, 6], [computador, 7, 7], [fundo_jogavel, 7, 8],
@@ -11,8 +12,6 @@ tabuleiro = [
     [jogador, 3, 1], [fundo_jogavel, 3, 2], [jogador, 3, 3], [fundo_jogavel, 3, 4], [jogador, 3, 5], [fundo_jogavel, 3, 6], [jogador, 3, 7], [fundo_jogavel, 3, 8],
     [fundo_jogavel, 2, 1], [jogador, 2, 2], [fundo_jogavel, 2, 3], [jogador, 2, 4], [fundo_jogavel, 2, 5], [jogador, 2, 6], [fundo_jogavel, 2, 7], [jogador, 2, 8],
     [jogador, 1, 1], [fundo_jogavel, 1, 2], [jogador, 1, 3], [fundo_jogavel, 1, 4], [jogador, 1, 5], [fundo_jogavel, 1, 6], [jogador, 1, 7], [fundo_jogavel, 1, 8]]
-
-posicao = 0
 for c in tabuleiro:
     if posicao != c[1]:
         print()
@@ -122,5 +121,3 @@ def jogada_1():
         posicao = c[1]
         print(c[0], end='')
 jogada_1()
-
-
