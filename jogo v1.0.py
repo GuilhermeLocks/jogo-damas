5fd675afabe7c8b26a394abf3ecd0ea4d963075e
Guilherme da Silva Locks <guisilvalocks@gmail.com>
2025-06-26 13:08:28 -0300

    dama

--- jogo v1.0.py
+++ jogo v1.0.py
@@ -13,115 +13,141 @@ tabuleiro = [
     [fundo_jogavel, 2, 1], [jogador, 2, 2], [fundo_jogavel, 2, 3], [jogador, 2, 4], [fundo_jogavel, 2, 5], [jogador, 2, 6], [fundo_jogavel, 2, 7], [jogador, 2, 8],
     [jogador, 1, 1], [fundo_jogavel, 1, 2], [jogador, 1, 3], [fundo_jogavel, 1, 4], [jogador, 1, 5], [fundo_jogavel, 1, 6], [jogador, 1, 7], [fundo_jogavel, 1, 8]]
 
-
-while True:
-    def jogada_1():
-        posicao = 0
-        for c in tabuleiro:
-            if posicao != c[1]:
-                print()
-            posicao = c[1]
-            print(c[0], end='')
-        print('')
-        print('')
-        posicao = 0
-        jogada_1 = 0
-        while jogada_1 == 0:
+def jogada_1():
+    posicao = 0
+    for c in tabuleiro:
+        if posicao != c[1]:
+            print()
+        posicao = c[1]
+        print(c[0], end='')
+    print('')
+    print('')
+    posicao = 0
+    jogada_1 = 0
+    while jogada_1 == 0:
+        while True:
             while True:
-                while True:
-                    try:
-                        posicao_x1 = int(input('x = '))
-                    except:
-                        print('Digite um valor valido')
-                    else:
-                        break
-                if posicao_x1 >= 0 and posicao_x1 <= 9:
-                    break
-                else:
+                try:
+                    posicao_x1 = int(input('x = '))
+                except:
                     print('Digite um valor valido')
+                else:
+                    break
+            if posicao_x1 >= 0 and posicao_x1 <= 9:
+                break
+            else:
+                print('Digite um valor valido')
+        while True:
             while True:
-                while True:
-                    try:
-                        posicao_y1 = int(input('y = '))
-                    except:
-                        print('Digite um valor valido')
-                    else:
-                        break
-                if posicao_y1 >= 0 and posicao_y1 <= 9:
-                    break
-                else:
+                try:
+                    posicao_y1 = int(input('y = '))
+                except:
                     print('Digite um valor valido')
-            for c in tabuleiro:
-                if c[1] == posicao_x1:
-                    if c[2] == posicao_y1:
-                        if c[0] == 'o  ':
-                            jogada_1 = c[:]
-                            c[0] = '\033[35m{}\033[0m'.format(c[0])
-                        else:
-                            print('')
-                            print('Posição invalida.')
-                            print('')
-        for c in tabuleiro:
-            if posicao != c[1]:
-                print()
-            posicao = c[1]
-            print(c[0], end='')
-        jogada_2 = 0
-        while jogada_2 == 0:
-            print('')
-            print('')
-            while True:
-                while True:
-                    try:
-                        posicao_x2 = int(input('x2 = '))
-                    except:
-                        print('Digite um valor valido')
-                    else:
-                        break
-                if posicao_x2 >= 1 and posicao_x2 <= 9:
-                    break
                 else:
-                    print('Digite um valor valido')
-            while True:
-                while True:
-                    try:
-                        posicao_y2 = int(input('y2 = '))
-                    except:
-                        print('Digite um valor valido')
-                    else:
-                        break
-                if posicao_y2 >= 1 and posicao_y2 <= 9:
                     break
-                else:
-                    print('Digite um valor valido')
-            for c in tabuleiro:
-                if c[0] == '*  ' and c[1] == posicao_x2 and c[2] == posicao_y2 and posicao_y1 + 2 == posicao_y2 and posicao_x1 + 2 == posicao_x2:
-                    jogada_2 = c[:]
-                    c[0] = '\033[34m{}\033[0m'.format(c[0])
-                if c[0] == '*  ' and c[1] == posicao_x2 and c[2] == posicao_y2 and posicao_y1 + 2 == posicao_y2 and posicao_x1 - 2 == posicao_x2:
-                    jogada_2 = c[:]
-                    c[0] = '\033[34m{}\033[0m'.format(c[0])
-                if c[0] == '*  ' and c[1] == posicao_x2 and c[2] == posicao_y2 and posicao_y1 - 2 == posicao_y2 and posicao_x1 - 2 == posicao_x2:
-                    jogada_2 = c[:]
-                    c[0] = '\033[34m{}\033[0m'.format(c[0])
-                if c[0] == '*  ' and c[1] == posicao_x2 and c[2] == posicao_y2 and posicao_y1 - 2 == posicao_y2 and posicao_x1 + 2 == posicao_x2:
-                    jogada_2 = c[:]
-                    c[0] = '\033[34m{}\033[0m'.format(c[0])
-
-
-                if c[0] == '*  ' and c[1] == posicao_x2 and c[2] == posicao_y2 and posicao_y1 - 1 == posicao_y2 and posicao_x1 + 1 == posicao_x2:
-                    jogada_2 = c[:]
-                    c[0] = '\033[34m{}\033[0m'.format(c[0])
-                if c[0] == '*  ' and c[1] == posicao_x2 and c[2] == posicao_y2 and posicao_y1 + 1 == posicao_y2 and posicao_x1 + 1 == posicao_x2:
-                    jogada_2 = c[:]
-                    c[0] = '\033[34m{}\033[0m'.format(c[0])
+            if posicao_y1 >= 0 and posicao_y1 <= 9:
+                break
+            else:
+                print('Digite um valor valido')
         for c in tabuleiro:
             if c[1] == posicao_x1:
                 if c[2] == posicao_y1:
-                    c[0] = '*  '
+                    if c[0] == 'o  ':
+                        jogada_1 = c[:]
+                        c[0] = '\033[35m{}\033[0m'.format(c[0])
+                    else:
+                        print('')
+                        print('Posição invalida.')
+                        print('')
+    for c in tabuleiro:
+        if posicao != c[1]:
+            print()
+        posicao = c[1]
+        print(c[0], end='')
+    jogada_2 = 0
+    while jogada_2 == 0:
+        print('')
+        print('')
+        while True:
+            while True:
+                try:
+                    posicao_x2 = int(input('x2 = '))
+                except:
+                    print('Digite um valor valido')
+                else:
+                    break
+            if posicao_x2 >= 1 and posicao_x2 <= 9:
+                break
+            else:
+                print('Digite um valor valido')
+        while True:
+            while True:
+                try:
+                    posicao_y2 = int(input('y2 = '))
+                except:
+                    print('Digite um valor valido')
+                else:
+                    break
+            if posicao_y2 >= 1 and posicao_y2 <= 9:
+                break
+            else:
+                print('Digite um valor valido')
         for c in tabuleiro:
-            if c[1] == posicao_x2:
-                if c[2] == posicao_y2:
-                    c[0] = 'o  '
+            if c[0] == '*  ' and c[1] == posicao_x2 and c[
+                2] == posicao_y2 and posicao_y1 + 2 == posicao_y2 and posicao_x1 + 2 == posicao_x2:
+                jogada_2 = c[:]
+                c[0] = '\033[34m{}\033[0m'.format(c[0])
+            if c[0] == '*  ' and c[1] == posicao_x2 and c[
+                2] == posicao_y2 and posicao_y1 + 2 == posicao_y2 and posicao_x1 - 2 == posicao_x2:
+                jogada_2 = c[:]
+                c[0] = '\033[34m{}\033[0m'.format(c[0])
+            if c[0] == '*  ' and c[1] == posicao_x2 and c[
+                2] == posicao_y2 and posicao_y1 - 2 == posicao_y2 and posicao_x1 - 2 == posicao_x2:
+                jogada_2 = c[:]
+                c[0] = '\033[34m{}\033[0m'.format(c[0])
+            if c[0] == '*  ' and c[1] == posicao_x2 and c[
+                2] == posicao_y2 and posicao_y1 - 2 == posicao_y2 and posicao_x1 + 2 == posicao_x2:
+                jogada_2 = c[:]
+                c[0] = '\033[34m{}\033[0m'.format(c[0])
 
+            if c[0] == '*  ' and c[1] == posicao_x2 and c[
+                2] == posicao_y2 and posicao_y1 - 1 == posicao_y2 and posicao_x1 + 1 == posicao_x2:
+                jogada_2 = c[:]
+                c[0] = '\033[34m{}\033[0m'.format(c[0])
+            if c[0] == '*  ' and c[1] == posicao_x2 and c[
+                2] == posicao_y2 and posicao_y1 + 1 == posicao_y2 and posicao_x1 + 1 == posicao_x2:
+                jogada_2 = c[:]
+                c[0] = '\033[34m{}\033[0m'.format(c[0])
+    for c in tabuleiro:
+        if c[1] == posicao_x1:
+            if c[2] == posicao_y1:
+                c[0] = '*  '
+    for c in tabuleiro:
+        if c[1] == posicao_x2:
+            if c[2] == posicao_y2:
+                c[0] = 'o  '
+
+
+
+def computador_1():
+    for c in tabuleiro:
+        if c[0] == 'x  ':
+            posicao_x = c[1]
+            posicao_y = c[2]
+            for v in tabuleiro:
+                if v[0] == 'o  'and v[1] == posicao_x-1 and v[2] == posicao_y-1:
+                    print(v)
+                    print(c)
+                    for c in tabuleiro:
+                        if c[1] == posicao_x:
+                            if c[2] == posicao_y:
+                                if c[0] == 'x  ':
+                                    c[0] = '\033[35m{}\033[0m'.format(c[0])
+
+
+
+
+
+while True:
     jogada_1()
+    computador_1()
