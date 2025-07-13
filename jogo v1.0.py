537b2a0ad5441d95dd573d380eaee1551e0e80fa
Guilherme da Silva Locks <guisilvalocks@gmail.com>
2025-07-12 23:46:16 -0300

    dama

--- jogo v1.0.py
+++ jogo v1.0.py
@@ -1,5 +1,5 @@
-fundo_jogavel = '=  '
-fundo_injogavel = '*  '
+fundo_jogavel = '*  '
+fundo_injogavel = '=  '
 jogador = 'o  '
 computador = 'x  '
 azul = '\033[34mx  \033[0m'
@@ -8,14 +8,14 @@ verde = '\033[32mx  \033[0m'
 vermelho = '\033[31mx  \033[0m'
 tabuleiro = [
     ['X  ', 9, 0],
-    ['8  ', 8, 0], [fundo_jogavel, 8, 1], [computador, 8, 2], [fundo_jogavel, 8, 3], [computador, 8, 4], [fundo_jogavel, 8, 5], [computador, 8, 6], [fundo_jogavel, 8, 7], [computador, 8, 8],
-    ['7  ', 7, 0], [fundo_injogavel, 7, 1], [fundo_jogavel, 7, 2], [jogador, 7, 3], [fundo_jogavel, 7, 4], [fundo_injogavel, 7, 5], [fundo_jogavel, 7, 6], [fundo_injogavel, 7, 7], [fundo_jogavel, 7, 8],
-    ['6  ', 6, 0], [fundo_jogavel, 6, 1], [fundo_injogavel, 6, 2], [fundo_jogavel, 6, 3], [fundo_injogavel, 6, 4], [fundo_jogavel, 6, 5], [fundo_injogavel, 6, 6], [fundo_jogavel, 6, 7], [fundo_injogavel, 6, 8],
-    ['5  ', 5, 0], [fundo_injogavel, 5, 1], [fundo_jogavel, 5, 2], [jogador, 5, 3], [fundo_jogavel, 5, 4], [fundo_injogavel, 5, 5], [fundo_jogavel, 5, 6], [fundo_injogavel, 5, 7], [fundo_jogavel, 5, 8],
-    ['4  ', 4, 0], [fundo_jogavel, 4, 1], [fundo_injogavel, 4, 2], [fundo_jogavel, 4, 3], [fundo_injogavel, 4, 4], [fundo_jogavel, 4, 5], [fundo_injogavel, 4, 6], [fundo_jogavel, 4, 7], [fundo_injogavel, 4, 8],
-    ['3  ', 3, 0], [jogador, 3, 1], [fundo_jogavel, 3, 2], [jogador, 3, 3], [fundo_jogavel, 3, 4], [jogador, 3, 5], [fundo_jogavel, 3, 6], [jogador, 3, 7], [fundo_jogavel, 3, 8],
-    ['2  ', 2, 0], [fundo_jogavel, 2, 1], [fundo_injogavel, 2, 2], [fundo_jogavel, 2, 3], [fundo_injogavel, 2, 4], [fundo_jogavel, 2, 5], [jogador, 2, 6], [fundo_jogavel, 2, 7], [jogador, 2, 8],
-    ['1  ', 1, 0], [jogador, 1, 1], [fundo_jogavel, 1, 2], [jogador, 1, 3], [fundo_jogavel, 1, 4], [jogador, 1, 5], [fundo_jogavel, 1, 6], [jogador, 1, 7], [fundo_jogavel, 1, 8],
+    ['8  ', 8, 0], [fundo_injogavel, 8, 1], [computador, 8, 2], [fundo_injogavel, 8, 3], [computador, 8, 4], [fundo_injogavel, 8, 5], [computador, 8, 6], [fundo_injogavel, 8, 7], [computador, 8, 8],
+    ['7  ', 7, 0], [fundo_jogavel, 7, 1], [fundo_injogavel, 7, 2], [jogador, 7, 3], [fundo_injogavel, 7, 4], [fundo_jogavel, 7, 5], [fundo_injogavel, 7, 6], [fundo_jogavel, 7, 7], [fundo_injogavel, 7, 8],
+    ['6  ', 6, 0], [fundo_injogavel, 6, 1], [fundo_jogavel, 6, 2], [fundo_injogavel, 6, 3], [fundo_jogavel, 6, 4], [fundo_injogavel, 6, 5], [fundo_jogavel, 6, 6], [fundo_injogavel, 6, 7], [fundo_jogavel, 6, 8],
+    ['5  ', 5, 0], [fundo_jogavel, 5, 1], [fundo_injogavel, 5, 2], [jogador, 5, 3], [fundo_injogavel, 5, 4], [fundo_jogavel, 5, 5], [fundo_injogavel, 5, 6], [fundo_jogavel, 5, 7], [fundo_injogavel, 5, 8],
+    ['4  ', 4, 0], [fundo_injogavel, 4, 1], [fundo_jogavel, 4, 2], [fundo_injogavel, 4, 3], [fundo_jogavel, 4, 4], [fundo_injogavel, 4, 5], [fundo_jogavel, 4, 6], [fundo_injogavel, 4, 7], [fundo_jogavel, 4, 8],
+    ['3  ', 3, 0], [jogador, 3, 1], [fundo_injogavel, 3, 2], [jogador, 3, 3], [fundo_injogavel, 3, 4], [jogador, 3, 5], [fundo_injogavel, 3, 6], [jogador, 3, 7], [fundo_injogavel, 3, 8],
+    ['2  ', 2, 0], [fundo_injogavel, 2, 1], [fundo_jogavel, 2, 2], [fundo_injogavel, 2, 3], [fundo_jogavel, 2, 4], [fundo_injogavel, 2, 5], [jogador, 2, 6], [fundo_injogavel, 2, 7], [jogador, 2, 8],
+    ['1  ', 1, 0], [jogador, 1, 1], [fundo_injogavel, 1, 2], [jogador, 1, 3], [fundo_injogavel, 1, 4], [jogador, 1, 5], [fundo_injogavel, 1, 6], [jogador, 1, 7], [fundo_injogavel, 1, 8],
     ['0  ', 0, 0],  ['1  ', 0, 0], ['2  ', 0, 0], ['3  ', 0, 0], ['4  ', 0, 0], ['5  ', 0, 0], ['6  ', 0, 0], ['7  ', 0, 0], ['8  ', 0, 0], ['Y  ', 0, 0],                                                                                                                                                     ]
 def tabuada():
     posicao = 0
