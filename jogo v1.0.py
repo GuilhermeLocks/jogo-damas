814a2528708fbfe9c9a5a25ed6af0f0a9a77a9aa
Guilherme da Silva Locks <guisilvalocks@gmail.com>
2025-08-01 14:46:22 -0300

    dama

--- jogo v1.0.py
+++ jogo v1.0.py
@@ -349,6 +349,37 @@ def computador():
             posicao_y = c[2]
 
             for c in tabuleiro:
+                if c[0] == 'o  ' and c[1] == posicao_x - 1 and c[2] == posicao_y - 5:
+                    for c in tabuleiro:
+                        if c[0] == '*  ' and c[1] == posicao_x - 2 and c[2] == posicao_y - 6:
+
+                            for c in tabuleiro:
+                                if c[1] == posicao_x:
+                                    if c[2] == posicao_y:
+                                        if c[0] in todos:
+                                            c[0] = vermelho
+            for c in tabuleiro:
+                if c[0] == 'o  ' and c[1] == posicao_x + 1 and c[2] == posicao_y - 5:
+                    for c in tabuleiro:
+                        if c[0] == '*  ' and c[1] == posicao_x + 2 and c[2] == posicao_y - 6:
+
+                            for c in tabuleiro:
+                                if c[1] == posicao_x:
+                                    if c[2] == posicao_y:
+                                        if c[0] in todos:
+                                            c[0] = vermelho
+            for c in tabuleiro:
+                if c[0] == 'o  ' and c[1] == posicao_x + 1 and c[2] == posicao_y - 3:
+                    for c in tabuleiro:
+                        if c[0] == '*  ' and c[1] == posicao_x + 2 and c[2] == posicao_y - 2:
+
+                            for c in tabuleiro:
+                                if c[1] == posicao_x:
+                                    if c[2] == posicao_y:
+                                        if c[0] in todos:
+                                            c[0] = vermelho
+
+            for c in tabuleiro:
                 if c[0] == 'o  ' and c[1] == posicao_x - 5 and c[2] == posicao_y - 3:
                     for c in tabuleiro:
                         if c[0] == '*  ' and c[1] == posicao_x - 6 and c[2] == posicao_y - 2:
@@ -379,7 +410,6 @@ def computador():
                                         if c[0] in todos:
                                             c[0] = vermelho
 
-
             for c in tabuleiro:
                 if c[0] == 'o  ' and c[1] == posicao_x - 5 and c[2] == posicao_y - 1:
                     for c in tabuleiro:
@@ -400,29 +430,16 @@ def computador():
                                     if c[2] == posicao_y:
                                         if c[0] in todos:
                                             c[0] = vermelho
-
-
             for c in tabuleiro:
-                if c[0] == 'o  ' and c[1] == posicao_x - 1 and c[2] == posicao_y - 5:
+                if c[0] == 'o  ' and c[1] == posicao_x - 3 and c[2] == posicao_y + 1:
                     for c in tabuleiro:
-                        if c[0] == '*  ' and c[1] == posicao_x - 2 and c[2] == posicao_y - 6:
+                        if c[0] == '*  ' and c[1] == posicao_x - 2 and c[2] == posicao_y + 2:
 
                             for c in tabuleiro:
                                 if c[1] == posicao_x:
                                     if c[2] == posicao_y:
                                         if c[0] in todos:
                                             c[0] = vermelho
-            for c in tabuleiro:
-                if c[0] == 'o  ' and c[1] == posicao_x + 1 and c[2] == posicao_y - 5:
-                    for c in tabuleiro:
-                        if c[0] == '*  ' and c[1] == posicao_x + 2 and c[2] == posicao_y - 6:
-
-                            for c in tabuleiro:
-                                if c[1] == posicao_x:
-                                    if c[2] == posicao_y:
-                                        if c[0] in todos:
-                                            c[0] = vermelho
-
 
             for c in tabuleiro:
                 if c[0] == 'o  ' and c[1] == posicao_x - 5 and c[2] == posicao_y + 3:
@@ -444,7 +461,6 @@ def computador():
                                     if c[2] == posicao_y:
                                         if c[0] in todos:
                                             c[0] = vermelho
-
 
             for c in tabuleiro:
                 if c[0] == 'o  ' and c[1] == posicao_x - 1 and c[2] == posicao_y + 5:
